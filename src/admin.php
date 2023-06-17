8b4f418d1b7a1f35965c5187c3495dc0d812d6f0
reinhardbuyabo <buyabor@gmail.com>
2023-06-17 22:39:59 +0300

    admin.php done

---
+++ src/admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+</head>
+
+<body>
+    <h2>Welcome, <?php echo $_SESSION['username']; ?></h2>
+    <p>This is the Admin page</p>
+</body>
+
+</html>
