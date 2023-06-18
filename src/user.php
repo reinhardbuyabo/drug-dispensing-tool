0f1f6dc62d3c83e22f24a2e5b6d6ecb3e7a1549d
reinhardbuyabo <buyabor@gmail.com>
2023-06-18 19:09:23 +0300

    User Page

---
+++ src/user.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['username']) || $_SESSION['user_type'] != 'patient') {
+    header("Location: login.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+</head>
+
+<body>
+    <h2>Welcome <?php echo $_SESSION['username']; ?></h2>
+    <p>This is the user page</p>
+</body>
+
+</html>
