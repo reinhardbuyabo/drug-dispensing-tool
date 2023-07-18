ae008cc61d36f68c288ecaff855b2e021670b6f7
reinhardbuyabo <buyabor@gmail.com>
2023-07-18 10:47:28 +0300

    feat: Empty

---
+++ src/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="../styles/styles.css">
+    <script src="https://kit.fontawesome.com/9e862feeff.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
