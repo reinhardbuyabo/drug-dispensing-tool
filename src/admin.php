da2a52ce23e76056a383793c4a82e5c2634f84e4
reinhardbuyabo <buyabor@gmail.com>
2023-07-21 21:02:35 +0300

    feat: Show history of drugs dispensed from the interface

--- src/admin.php
+++ src/admin.php
@@ -31,18 +31,12 @@
             <p><span>Note:</span> This page is for authorized users only!</p>
 
             <div>
-                <button type="button"><span></span>ADD USER</button>
+                <button type="button"><span></span><a href="register.php?user_type=patient">ADD USER</a></button>
                 <button type="button"><span></span><a href="add_drug.php">ADD DRUG</a></button>
             </div>
         </div>
     </div>
 
-    <!-- <p>This is the Admin Page</p>
-
-    <a href="./display_users.php">Display All Users</a>
-
-    <a href="./doctor_form.html">Add A Doctor</a>
-    <a href="./pharmacist_form.html">Add A Pharmacist</a> -->
 </body>
 
 </html>
