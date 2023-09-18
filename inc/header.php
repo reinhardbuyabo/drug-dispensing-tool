a26fd8b16fa51d526208b0f2799955f3d27760f8
reinhardbuyabo <buyabor@gmail.com>
2023-09-18 13:48:24 +0300

    2.2

---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title><?php echo 'Home'; ?></title>
+    <link rel="stylesheet" href="../styles/major_styles.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="wrapper">
+            <h1 class="branding-title"><a href="./">Home</a></h1>
+
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+    </div>
