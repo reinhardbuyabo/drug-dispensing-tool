ece045a132c6571299c4ee79777f1b952921f97c
reinhardbuyabo <buyabor@gmail.com>
2023-11-29 11:42:30 +0300

    Initialized Redux Store

---
+++ frontend/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+/**
+ * STORE
+ * The current Redux application state lives in an object called the store .
+   The store is created by passing in a reducer, and has a method called getState that returns the current state value
+    */
+export const store = configureStore({
+    reducer: {
+        
+    }
+}); 
